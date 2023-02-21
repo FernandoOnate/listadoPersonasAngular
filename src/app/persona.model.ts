81ccf1e03729fadc5d3ee20305f548ad26d0fa69
Fernando Oñate Rodriguez <ofertar.fernando@gmail.com>
2023-02-21 14:58:26 -0500

    - Creada la clase persona y arreglado el index html

---
+++ src/app/persona.model.ts
@@ -0,0 +1,14 @@
+export class Persona {
+    /*
+    nombre:string = '';
+    apellido:string = '';
+    constructor(nom:string,ape:string){
+        this.nombre = nom;
+        this.apellido = ape;
+    }
+    */
+    // PARA ESTO PODEMOS USAR LA SINTAXIS SIMPLIFICADA Y DA LO MISMO
+    constructor(public nombre: string, public apellido: string) {
+
+    }
+}
