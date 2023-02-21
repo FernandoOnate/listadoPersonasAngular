49061ee0c770ee4c231c60459b41279df6e7d423
Fernando Oñate Rodriguez <ofertar.fernando@gmail.com>
2023-02-21 15:27:44 -0500

    - Configurado el form - Configurado two-way binding - Configurado mensaje - Proyecto listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,25 @@ import { Persona } from './persona.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo = 'listado-personas';
-  personas:Persona[] = [
-    new Persona('Juanito','Alcachofas'),
-    new Persona('Pepito','Perez'),
-    new Persona('Lorena','Castro')
+  titulo = 'Listado de personas';
+  apellidoInput = '';
+  nombreInput = '';
+  mensaje = '';
+  showMessage = false;
+  personas: Persona[] = [
+    new Persona('Juanito', 'Alcachofas'),
+    new Persona('Pepito', 'Perez'),
+    new Persona('Lorena', 'Castro')
   ];
+  agregarPersona(): void {
+    if (this.apellidoInput.length > 0 && this.nombreInput.length > 0) {
+      const personaNueva = new Persona(this.nombreInput, this.apellidoInput);
+      this.personas.push(personaNueva);
+      this.showMessage = true;
+      this.mensaje = 'Persona agregada!';
+    }else{
+      this.mensaje = 'Debes completar los campos!';
+    }
+
+  }
 }
