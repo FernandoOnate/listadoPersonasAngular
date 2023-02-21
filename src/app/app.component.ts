e17fa5c9e194c0420e6db2acdf3e48c887574572
Fernando Oñate Rodriguez <ofertar.fernando@gmail.com>
2023-02-21 15:05:41 -0500

    - Creado array de tipo Personas - html muestra el array con la directiva ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   titulo = 'listado-personas';
+  personas:Persona[] = [
+    new Persona('Juanito','Alcachofas'),
+    new Persona('Pepito','Perez'),
+    new Persona('Lorena','Castro')
+  ];
 }
