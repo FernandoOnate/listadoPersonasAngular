37763cce93b3936afff93333f5c96d8d2b376715
Fernando Oñate Rodriguez <ofertar.fernando@gmail.com>
2023-02-21 18:58:25 -0500

    - Clase servicio dentro de otro servicio

--- src/app/personas.service.ts
+++ src/app/personas.service.ts
@@ -1,13 +1,18 @@
+import { Injectable } from "@angular/core";
+import { LogginService } from "./logginService.service";
 import { Persona } from "./persona.model";
-
+// es necesario este decorador para hacerle saber a angular que 
+// este servicio se puede dejar inyectar por otro servicio
+@Injectable()
 export class PersonaService {
+    constructor(private loginService: LogginService) { }
     personas: Persona[] = [
         new Persona('Juanito', 'Alcachofas'),
         new Persona('Pepito', 'Perez'),
         new Persona('Lorena', 'Castro')
     ];
-
     agregarPersona(persona: Persona): void {
         this.personas.push(persona);
+        this.loginService.enviaMensajeConsola('La persona se agregó: ' + persona.nombre + ' ' + persona.apellido);
     }
 }
