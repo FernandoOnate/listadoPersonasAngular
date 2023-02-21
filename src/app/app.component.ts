40b2d554c8c0ac81570b534b3c792d3b31166ba4
Fernando Oñate Rodriguez <ofertar.fernando@gmail.com>
2023-02-21 16:32:20 -0500

    - creado componente hijo formulario - conectado componente hijo con padre - hijo le manda a padre el objeto - padre pushea el objeto al array - Clase: event binding desde componente hijo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,12 @@ import { Persona } from './persona.model';
 })
 export class AppComponent {
   titulo = 'Listado de personas';
-  apellidoInput = '';
-  nombreInput = '';
-  mensaje = '';
-  showMessage = false;
   personas: Persona[] = [
     new Persona('Juanito', 'Alcachofas'),
     new Persona('Pepito', 'Perez'),
     new Persona('Lorena', 'Castro')
   ];
-  agregarPersona(): void {
-    if (this.apellidoInput.length > 0 && this.nombreInput.length > 0) {
-      const personaNueva = new Persona(this.nombreInput, this.apellidoInput);
-      this.personas.push(personaNueva);
-      this.showMessage = true;
-      this.mensaje = 'Persona agregada!';
-    }else{
-      this.mensaje = 'Debes completar los campos!';
-    }
-
+  personaEmitida(event:Persona):void{
+    this.personas.push(event);
   }
 }
---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Persona } from '../persona.model';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent {
+  @Output() personaCreada = new EventEmitter<Persona>();
+  nombreInput = '';
+  apellidoInput = '';
+  mensaje = '';
+  showMessage = false;
+
+  
+  agregarPersona(): void {
+    if (this.apellidoInput.length > 0 && this.nombreInput.length > 0) {
+      const personaNueva = new Persona(this.nombreInput, this.apellidoInput);
+      // this.personas.push(personaNueva);
+      this.personaCreada.emit(personaNueva);
+      this.showMessage = true;
+      this.mensaje = 'Persona agregada!';
+    }else{
+      this.mensaje = 'Debes completar los campos!';
+    }
+  }
+}
