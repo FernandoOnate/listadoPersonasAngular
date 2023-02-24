3cbe70bc49efe85e22ab019a05538227dad79748
Fernando Oñate Rodriguez <ofertar.fernando@gmail.com>
2023-02-24 18:11:08 -0500

    - Creado servicio de login - Creado componente login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PersonasComponent } from './personas/personas.component';
 import { FormularioComponent } from './personas/formulario/formulario.component';
 import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
@@ -24,6 +25,11 @@ const routes: Routes = [
       }
     ]
   },
+  {
+    path:'login',
+    component: LoginComponent
+  }
+  ,
   {
     path: '**',
     component: ErrorComponent
